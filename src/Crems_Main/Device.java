955e37a77bf1ee8414e9e2d8f9ac22d76575f858
Niel Marc Avelino <185757025+Greatsquish@users.noreply.github.com>
2026-02-13 00:03:46 +0800

    sasas
    asas

--- src/Crems_Main/Device.java
+++ src/Crems_Main/Device.java
@@ -8,36 +8,6 @@ package Crems_Main;
  *
  * @author NIEL
  */
-public class Device extends Resource {
+public class Device {
     
-    private String deviceType; // "Computer", "Tablet", "Laptop"
-    private String specs; // basic specs
-    
-    public Device(String resourceId, String name, int maxUsageTime, 
-                  String deviceType, String specs) {
-        super(resourceId, name, "Device", maxUsageTime);
-        this.deviceType = deviceType;
-        this.specs = specs;
-    }
-    
-    public String getDeviceType() {
-        return deviceType;
-    }
-    
-    public void setDeviceType(String deviceType) {
-        this.deviceType = deviceType;
-    }
-    
-    public String getSpecs() {
-        return specs;
-    }
-    
-    public void setSpecs(String specs) {
-        this.specs = specs;
-    }
-    
-    @Override
-    public String toString() {
-        return getName() + " [" + deviceType + "] - " + getStatus() + " (" + specs + ")";
-    }
 }
